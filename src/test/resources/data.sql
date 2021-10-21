9a18816fc3302cb5d5f098df19c49c9518ec7762
devlos0322 <devlos0322@gmail.com>
2021-10-21 21:08:30 +0900

    Update User class remark about test data

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,6 +1,6 @@
+-- 테스트용 임시 저장 DB를 만드는 것이다.
 -- hibernates의 시퀀스에 현재 id값을 알려줌
 call next value for hibernate_sequence;
-
 insert into user (`id`, `name`, `email`, `created_at`, `updated_at`) values (1, 'devlos0', 'devlos@gmail.com', now(), now());
 
 call next value for hibernate_sequence;
