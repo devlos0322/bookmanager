8d9270104b224cbecf5232aca4a179e435ad3e3e
devlos0322 <devlos0322@gmail.com>
2021-10-22 23:42:34 +0900

    Add where cases using By keywords.

--- src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
@@ -3,8 +3,34 @@ package com.fastcampus.jpa.bookmanager.repository;
 import com.fastcampus.jpa.bookmanager.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 //제너릭은 엔티티
 //JPA 인터페이스를 만드는것많으로 많은 기능을 쓸 수 있음
 public interface UserRepository extends JpaRepository<User, Long> {
+    List<User> findByName(String name);
 
+    User findByEmail(String email);
+
+    User getByEmail(String email);
+
+    User readByEmail(String email);
+
+    User queryByEmail(String email);
+
+    User searchByEmail(String email);
+
+    User streamByEmail(String email);
+
+    User findUserByEmail(String email);
+
+    User findSomethingByEmail(String email);
+
+    //User findByByEmail(String email);
+
+    List<User> findFirst2ByName(String name);
+    List<User> findTop2ByName(String name);
+
+    //이녀석은 키워드가 없기 때문에 findByName으로 검색된다.
+    List<User> findLast1ByName(String name);
 }
--- src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -22,58 +22,63 @@ class UserRepositoryTest {
     @Test
 //    @Transactional //getOne을 쓸경우 필요함 Lazy fatch를 지원함
     void crud() {   //create, read, update, delete
-    /* 1번
-        //        userRepository.save(new User()); //테이블에 저장하는것
-
-        //System.out.println(">>> " + userRepository.findAll());  //User 테이블의 모든 정보를 출력
+/*
+        //1번
+        userRepository.save(new User()); //테이블에 저장하는것
         userRepository.findAll().forEach(System.out::println);  //Java Lambda
         //람다와 같음
-//        for (User user: userRepository.findAll()) {
-//            System.out.println(user);
-//        }
-    */
-//        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
-
+        for (User user: userRepository.findAll()) {
+            System.out.println(user);
+        }
+        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
+*/
 
         //1번, 3번, 5번만 호출하기
-    /* 2번
+/*
+        // 2번
         List<User> users = userRepository.findAllById(Lists.newArrayList(1L,3L,5L));
-
         users.forEach(System.out::println);
-    */
+*/
 
-    /* 3번
+/*
+        // 3번
         User user1 = new User("Jack", "jack@fast.com");
         User user2 = new User("Mason", "Mason@fast.com");
         userRepository.saveAll(Lists.newArrayList(user1,user2));
         List<User> users = userRepository.findAll();
         users.forEach(System.out::println);
-    */
+*/
         // 즉시 로딩과 지연 로딩 차이 확인하기
-    /* 4번
+/*
+        // 4번
         User user = userRepository.findById(1L).orElse(null);
         System.out.println(user);
-    */
-    /* 5번
-//        userRepository.save(new User("new devlos", "devlos_new@gmail.com"));
-//        userRepository.flush(); //데이터베이스와 JPA의 데이터를 동기화 시킴
+*/
+
+/*
+        // 5번
+        userRepository.save(new User("new devlos", "devlos_new@gmail.com"));
+        userRepository.flush(); //데이터베이스와 JPA의 데이터를 동기화 시킴
         userRepository.saveAndFlush(new User("new devlos", "devlos_new@gmail.com"));
         userRepository.findAll().forEach(System.out::println);
-    */
-    /*  6번 count로 exists 처리
+*/
+
+/*
+        // 6번 count로 exists 처리
         boolean exists = userRepository.existsById(1L);
         System.out.println(exists);
-    */
-//        userRepository.delete(userRepository.findById(1L).orElseThrow(RuntimeException::new));
-        //userRepository.deleteAll(userRepository.findAllById((Lists.newArrayList(1L,3L))));
+        userRepository.delete(userRepository.findById(1L).orElseThrow(RuntimeException::new));
         // 쿼리상의 오베헤드 발생 . select -> delete가 수행되므로. 또한 Delete쿼리가 삭제하는만큼 생김
-// 7번
+        userRepository.deleteAll(userRepository.findAllById((Lists.newArrayList(1L,3L))));
+
 /*
+        // 7번
         // 쿼리 한번으로 전체 실행
         userRepository.deleteAllInBatch();
         userRepository.findAll().forEach(System.out::println);
 */
-/* 8번
+/*
+        // 8번
         Page<User> users = userRepository.findAll(PageRequest.of(0, 3));
         System.out.println("page : " + users);
         System.out.println("totalElements : " + users.getTotalElements());
@@ -81,19 +86,18 @@ class UserRepositoryTest {
         System.out.println("numberOfElements : " + users.getNumberOfElements());
         System.out.println("sort :" + users.getSort());
         System.out.println("sort :" + users.getSort());
-
         users.getContent().forEach(System.out::println);
-
  */
         //contains() => 양방향 매처
         //QEE -> Query by example
-        /*
+/*
+
         ExampleMatcher matcher = ExampleMatcher.matching()
                 .withIgnorePaths("name")    //무시
                 .withMatcher("email", endsWith());
         Example<User> example = Example.of(new User("ma", "devlos@gmail.com"),matcher);
         userRepository.findAll(example).forEach(System.out::println);
-        */
+*/
         //Update
         userRepository.save(new User("devlos", "devlos0322@gmail.com"));
         User user = userRepository.findById(1L).orElseThrow(RuntimeException::new);
